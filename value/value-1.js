d7d398fa69539141b7c3c2e6844f125f8b8e844c
Dantss626 <dantss626@gmail.com>
2020-01-09 12:00:51 +0800

    add files

---
+++ value/value-1.js
@@ -0,0 +1,10 @@
+/**
+ * Type of value
+ */
+
+//  What is the type of 2?
+function Question1() {
+    // enter the type below
+    const type = "";
+    console.log(`Your answer is ${typeof 2 === type ? "correct" : "wrong"}.`);
+}
